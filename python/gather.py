eb8b2630047542d5b76f3ae8572821d23c007352
James W. McClain <jmcclain@daystrom-data-concepts.com>
2020-01-14 18:36:15 -0500

    Clip to Bounds

--- python/gather.py
+++ python/gather.py
@@ -41,12 +41,13 @@ import scipy.ndimage
 
 def cli_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', required=True, type=str)
-    parser.add_argument('--index', required=True, type=int)
-    parser.add_argument('--output-path', required=True, type=str)
-    parser.add_argument('--sentinel-path', required=True, type=str)
+    parser.add_argument('--bounds', required=False, nargs='+', type=float)
     parser.add_argument('--delete', required=False,
                         default=True, type=ast.literal_eval)
+    parser.add_argument('--index', required=True, type=int)
+    parser.add_argument('--name', required=True, type=str)
+    parser.add_argument('--output-path', required=True, type=str)
+    parser.add_argument('--sentinel-path', required=True, type=str)
     return parser
 
 
@@ -97,19 +98,15 @@ if __name__ == '__main__':
             codes.append(os.system('rm -f /tmp/*.jp2 /tmp/CLD_20m.tif'))
 
     # Apply mask to create second scratch file
+    MASK_INDEX = 14-1
     if has_mask:
         with rio.open('/tmp/scratch0.tif') as ds:
             profile = copy.copy(ds.profile)
             data = ds.read()
-        MASK_INDEX = 14-1
         data[MASK_INDEX] = (data[MASK_INDEX] == 0)
         for i in range(0, MASK_INDEX):
             data[i] = data[i] * data[MASK_INDEX]
         data[MASK_INDEX] = data[MASK_INDEX] * args.index
-        if args.delete:
-            codes.append(os.system('rm -f /tmp/scratch0.tif'))
-        with rio.open('/tmp/scratch1.tif', 'w', **profile) as ds:
-            ds.write(data)
     else:
         with rio.open('/tmp/scratch0.tif') as ds:
             profile = copy.copy(ds.profile)
@@ -119,14 +116,19 @@ if __name__ == '__main__':
         mask = np.ones(xy) * args.index
         mask = mask[np.newaxis].astype(data.dtype)
         data = np.concatenate([data, mask], axis=0)
-        if args.delete:
-            codes.append(os.system('rm -f /tmp/scratch0.tif'))
-        with rio.open('/tmp/scratch1.tif', 'w', **profile) as ds:
-            ds.write(data)
+    if args.delete:
+        codes.append(os.system('rm -f /tmp/scratch0.tif'))
+    with rio.open('/tmp/scratch1.tif', 'w', **profile) as ds:
+        ds.write(data)
 
     # Warp and compress to create final file
+    if args.bounds is None or len(args.bounds) != 4:
+        te = ''
+    else:
+        [xmin, ymin, xmax, ymax] = args.bounds
+        te = '-te {} {} {} {}'.format(xmin, ymin, xmax, ymax)
     codes.append(os.system(
-        'gdalwarp -srcnodata 0 -dstnodata 0 -t_srs epsg:4326 /tmp/scratch1.tif -co COMPRESS=DEFLATE -co PREDICTOR=2 -co TILED=YES -co SPARSE_OK=YES {}'.format(filename)))
+        'gdalwarp -srcnodata 0 -dstnodata 0 -t_srs epsg:4326 /tmp/scratch1.tif -co COMPRESS=DEFLATE -co PREDICTOR=2 -co TILED=YES -co SPARSE_OK=YES {} {}'.format(te, filename)))
     if args.delete:
         codes.append(os.system('rm -f /tmp/scratch1.tif'))
 
