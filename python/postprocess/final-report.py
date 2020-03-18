0d632184699dabfb52c8f770ef756f95c13fcd20
James W. McClain <jmcclain@daystrom-data-concepts.com>
2020-03-18 12:32:37 -0400

    Write Compressed Final Reports

--- python/postprocess/final-report.py
+++ python/postprocess/final-report.py
@@ -28,13 +28,20 @@
 
 import argparse
 import copy
+import gzip
 import json
 
 
 def cli_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input-json', required=True, type=str)
-    parser.add_argument('--input-geojson', required=True, type=str)
+    parser.add_argument('--input-json', help='The response',
+                        required=True, type=str)
+    parser.add_argument('--scramble-json',
+                        help='The scramble response', required=False, type=str)
+    parser.add_argument('--scramble-index',
+                        help='The scramble index', required=False, type=int)
+    parser.add_argument('--input-geojson',
+                        help='The source report', required=True, type=str)
     parser.add_argument('--output', required=True, type=str)
     return parser
 
@@ -45,19 +52,34 @@ if __name__ == '__main__':
     with open(args.input_json, 'r') as f:
         data_json = json.load(f)
 
+    if args.scramble_json is not None and args.scramble_index is not None:
+        with open(args.scramble_json, 'r') as f:
+            data_json2 = json.load(f)
+
     with open(args.input_geojson, 'r') as f:
         data_geojson = json.load(f)
 
     for feature in data_geojson.get('features'):
         properties = feature.get('properties')
-        index = int(properties.get('DN')) - 1
-        if index >= 0:
-            properties['id'] = copy.copy(
-                data_json.get('selections')[index].get('id'))
-            for (k, v) in copy.copy(data_json.get('selections')[index].get('sceneMetadata')).items():
-                properties[k] = v
+        index = int(properties.get('DN'))
+        if (args.scramble_index is not None) and (index > args.scramble_index//2):
+            index = index - args.scramble_index
+            if index >= 0:
+                properties['id'] = copy.copy(
+                    data_json2.get('selections')[index].get('id'))
+                for (k, v) in copy.copy(data_json2.get('selections')[index].get('sceneMetadata')).items():
+                    properties[k] = v
+        else:
+            index = index - 1
+            if index >= 0:
+                properties['id'] = copy.copy(
+                    data_json.get('selections')[index].get('id'))
+                for (k, v) in copy.copy(data_json.get('selections')[index].get('sceneMetadata')).items():
+                    properties[k] = v
         del properties['DN']
 
-    with open(args.output, 'w') as f:
-        json.dump(data_geojson, f, sort_keys=True,
-                  indent=4, separators=(',', ': '))
+    with gzip.open(args.output, 'w') as f:
+        json_str = json.dumps(data_geojson, sort_keys=True,
+                              indent=4, separators=(',', ': ')) + '\n'
+        json_data = json_str.encode()
+        f.write(json_data)
